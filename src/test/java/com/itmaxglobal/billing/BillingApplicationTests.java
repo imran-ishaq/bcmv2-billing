e8d6389d2446badef68d2ee12ec2ad8bcfbf061b
imran.ishaq <imran.ishaq7071@gmail.com>
2024-09-05 20:27:10 +0500

    Initial commit

--- src/test/java/com/itmaxglobal/billing/BillingApplicationTests.java
+++ src/test/java/com/itmaxglobal/billing/BillingApplicationTests.java
@@ -3,7 +3,6 @@ package com.itmaxglobal.billing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BillingApplicationTests {
 
 	@Test
